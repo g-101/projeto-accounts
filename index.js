57ebdf2deda4ed9fa67f5676bafd41c035fb45b6
g101 <gyreplicant@gmail.com>
2022-04-29 10:25:22 -0300

    finalizando criacao de conta

--- index.js
+++ index.js
@@ -5,12 +5,51 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 
-console.log("Iniciamos o Accounts\n")
-
 // criando a conta
-function createAccount() {
+function buildAcc() {
+    inquirer.prompt([
+        {
+            name: "accName",
+            message: "Digite um nome para a sua conta:",
+        },
+    ])
+    .then((answer) => {
+        const accName = answer["accName"]
+        console.info(`\n\rNome escolhido: ${accName}`) // mostra o nome da conta
+        
+        // verifica se o banco de dados accounts existe
+        if(!fs.existsSync("accounts")) {
+            // se não existir ele cria o diretorio
+            fs.mkdirSync("accounts")
+        }
+
+        // validar o nome da conta, para saber se ja existe
+        // as contas são salvas em json
+        if(fs.existsSync(`accounts/${accName}.json`)) {
+            console.log(chalk.bgRed.black("Esta conta ja existe"))
+            buildAcc() // chama a criação de conta novamente
+            return
+        }
+        // conta criada com sucesso
+        fs.writeFileSync(
+            `accounts/${accName}.json`, 
+            '{"balance": 0}', 
+            function(err) {
+                console.log(err)
+            },
+        )     
+        console.log(chalk.green("Conta criada com sucesso\n\r"))
+        //chama para o menu principal
+        operation()
+    })
+    .catch((err) => console.log(err))
+}
+
+// prompt de criação de conta
+function showCreateAcc() {
     console.log(chalk.bgGreen.black("Parabéns por escolher nosso Banco!"))
     console.log(chalk.green("Defina suas opções de conta a seguir:"))
+    buildAcc()
 }
 
 // operacoes disponiveis para o cliente
@@ -31,8 +70,9 @@ function operation() {
         const action = answer["action"] // vai pegar a escolha do usuario
         // console.log(action)
         if(action === "Criar Conta") {
-            createAccount()
+            showCreateAcc()
         }
+        
     })
     .catch((err) => console.log(err))
 }
